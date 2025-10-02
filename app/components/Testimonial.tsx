671364a7575a39a94b193e142e679018fa9c1b11
viratpatel1 <viratpatel993@gmail.com>
2025-10-02 17:44:13 +0530

    feedback

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { FaUserTie } from 'react-icons/fa6';
 
 export default function TestimonialSection() {
